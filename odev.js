a009d2545fc519d05964ab8e8a8caf886e727e88
Aykut Şahin <sahinaykkt@gmail.com>
2020-12-31 19:21:21 +0000

    Düzenleme.

--- odev.js
+++ odev.js
@@ -1,5 +1,7 @@
 import data from "./data.js"
-import {createTableElements} from "./main.js";
+import {
+  createTableElements
+} from "./main.js";
 
 /*
   ALWAYS USE IMPORTED data ARRAY TO MAKE MANIPULATIONS
@@ -27,44 +29,44 @@ import {createTableElements} from "./main.js";
 
 /* RESET ACTION */
 document.querySelector("#reset").addEventListener("click", () => {
-    createTableElements(data, "allcities");
-    createTableElements([], "singlecity")
+  createTableElements(data, "allcities");
+  createTableElements([], "singlecity")
 });
 
 /* START CODING HERE */
 
 // Cities which are population bigger than 500.000.
 const biggerButton = document.querySelector("#populationBigger");
-biggerButton.addEventListener("click", (cities) => {
-    const bigger500 = data.filter(cities => cities.population > 500000);
-    createTableElements(bigger500,"allcities");
+biggerButton.addEventListener("click", () => {
+  const bigger500 = data.filter(cities => cities.population > 500000);
+  createTableElements(bigger500, "allcities");
 });
 
 // Cities which are land area less than 1000.
 const lessButton = document.querySelector("#landAreaLess");
-lessButton.addEventListener("click", cities => {
-    const less1000 = data.filter(cities => cities.landArea < 1000);
-    createTableElements(less1000,"allcities");
+lessButton.addEventListener("click", () => {
+  const less1000 = data.filter(cities => cities.landArea < 1000);
+  createTableElements(less1000, "allcities");
 });
 
 // Does any city has population less than 100.000?
 const alertLess100 = document.querySelector("#isPopulationLess");
-alertLess100.addEventListener("click", (cities) => {
-    const less100 = data.filter(cities => cities.population < 100000);
-    if(less100.length > 0) {
-      return alert("Yes");
-    } else {
-      return alert("No");
-    };
+alertLess100.addEventListener("click", () => {
+  const less100 = data.filter(cities => cities.population < 100000);
+  if (less100.length > 0) {
+    return new Toast("Yes");
+  } else {
+    return new Toast("No");
+  };
 });
 
 // Does any city has land area bigger than 100?
 const alertBigger100 = document.querySelector("#isLandBigger");
-alertBigger100.addEventListener("click", (cities) => {
-    const bigger100 = data.filter(cities => cities.landArea > 100);
-    if(bigger100.length > 0) {
-      return alert("Yes");
-    } else {
-      return alert("No");
-    };
+alertBigger100.addEventListener("click", () => {
+  const bigger100 = data.filter(cities => cities.landArea > 100);
+  if (bigger100.length > 0) {
+    return new Toast("Yes");
+  } else {
+    return new Toast("No");
+  };
 });
